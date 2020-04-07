a22269351afba6048d7f1e1c565abee8f3aa13e1
AllanTumu <allantumu1962@gmail.com>
2020-04-07 22:16:59 +0300

    change of user profile.

---
+++ app/src/main/java/com/example/ugtunda/Prevalent/Prevalent.java
@@ -0,0 +1,10 @@
+package com.example.ugtunda.Prevalent;
+
+import com.example.ugtunda.Models.Users;
+
+public class Prevalent {
+    public static Users currentOnlineUser;
+
+    public static final String USER_PHONE_KEY = "UserPhone";
+    public static final String USER_PASSWORD_KEY = "UserPassword";
+}
