f92faf3a71c6fdfb486c3a72cac65780ccdc1237
AllanTumu <allantumu1962@gmail.com>
2020-04-17 13:30:40 +0300

    Added the Admin maintain page where Admin can change the product details

--- app/src/main/java/com/example/ugtunda/AdminCategoryActivity.java
+++ app/src/main/java/com/example/ugtunda/AdminCategoryActivity.java
@@ -13,7 +13,7 @@ public class AdminCategoryActivity extends AppCompatActivity {
     private ImageView tshirts, sportsTshirts, femaleDresses, sweathers;
     private ImageView glasses, hatsCaps, walletsBagsPurses, shoes;
     private ImageView headPhonesHandsFree, Laptops,watches, mobilePhones;
-    private Button logoutBtn, checkOrdersBtn;
+    private Button logoutBtn, checkOrdersBtn, maintainProductsBtn;
 
 
     @Override
@@ -23,6 +23,18 @@ public class AdminCategoryActivity extends AppCompatActivity {
 
         logoutBtn = (Button) findViewById(R.id.admin_logout_btn);
         checkOrdersBtn = (Button) findViewById(R.id.check_orders_btn);
+        maintainProductsBtn = (Button) findViewById(R.id.maintain_btn);
+
+        maintainProductsBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent intent = new Intent(AdminCategoryActivity.this, HomeActivity.class);
+                intent.putExtra("Admin", "Admin");
+                startActivity(intent);
+
+            }
+        });
 
         logoutBtn.setOnClickListener(new View.OnClickListener() {
             @Override
